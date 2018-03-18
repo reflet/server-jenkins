4a90c77b9de14bdb03a94eeb1b667a23950a3653
reflet <reflet.jp@gmail.com>
2018-03-18 19:11:26 +0900

    environmentの定義を簡略化する

---
+++ selenium/qiita-view.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+driver = webdriver.Remote(
+    command_executor='http://selenium-hub:4444/wd/hub',
+    desired_capabilities=DesiredCapabilities.CHROME)
+driver.get("http://qiita.com/reflet")
+driver.close()
